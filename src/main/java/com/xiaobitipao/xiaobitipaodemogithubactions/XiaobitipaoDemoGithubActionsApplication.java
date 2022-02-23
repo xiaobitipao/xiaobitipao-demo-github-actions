96733b0b612e5ec668d0296098f82d213ab56c15
lzzheng <lzzheng@cn.ibm.com>
2022-02-23 13:25:59 +0800

    init by spring initializr

---
+++ src/main/java/com/xiaobitipao/xiaobitipaodemogithubactions/XiaobitipaoDemoGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.xiaobitipao.xiaobitipaodemogithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaobitipaoDemoGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaobitipaoDemoGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaobitipao/xiaobitipaodemogithubactions/XiaobitipaoDemoGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaobitipao.xiaobitipaodemogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaobitipaoDemoGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
